c7ac75a0f5920453f4c8c168d850bb1c5d96fdea
sristy verma <sristyel2016@gmail.com>
2023-01-20 16:43:17 +0530

    added reducerproduct

--- myecomxpress/src/context/productcontext.js
+++ myecomxpress/src/context/productcontext.js
@@ -1,12 +1,31 @@
-import {createContext, useContext, useEffect} from 'react';
+import {createContext, useContext, useReducer,useEffect} from 'react';
 import axios from 'axios'
+import reducer from '../reducer/productReducer'
 export const AppContext=createContext();
 const API="https://api.pujakaitem.com/api/products"
+
+
+const initialState={
+    isLoading:false,
+    isError:false,
+    products:[],
+    featureProducts:[],
+}
 export const AppProvider=({children})=>{
+
+const [state,dispatch]=useReducer(reducer,initialState)
+
+
     const getProducts=async (url)=>{
-        const res=await axios.get(url)
-        const products=await res.data
-        console.log(products)
+        dispatch({type:"SET_LOADING"})
+        try {
+            const res=await axios.get(url)
+            const products=await res.data
+            // console.log(products)
+            dispatch({type:"SET_API_DATA",payload:products})
+        } catch (error) {
+            dispatch({type:"Api_error"})
+        }
     }
 useEffect(()=>{
   getProducts(API)
@@ -14,7 +33,7 @@ useEffect(()=>{
   
 
 return (
-<AppContext.Provider value={{names:"sris"}}>
+<AppContext.Provider value={{...state}}>
     {children}
     </AppContext.Provider>
     )
